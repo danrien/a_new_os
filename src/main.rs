c4ee38d88a2b686ef5f71a3bfee59f2c37dd03ea
dvedvick@gmail.com <dvedvick@gmail.com>
2018-12-14 07:46:19 -0600

    Implement custom println macros

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,16 @@
 use core::panic::PanicInfo;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
-
-static HELLO: &[u8] = b"Hello World!";
 
 // Linux startup:
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    vga_buffer::print_something();
+    println!("Hello {}!", "Bobby");
+    // panic!("Some panic message");
 
     loop {}
 }
